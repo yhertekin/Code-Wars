2430ffd38baf74c9ff257534c192c7064032aaf5
yhertekin <yhertekin@gmail.com>
2020-02-17 15:32:26 +0300

    add grosshopper grade book

--- Python/8-KYU/Grasshopper - Grade book.py
+++ Python/8-KYU/Grasshopper - Grade book.py
@@ -19,3 +19,7 @@ def get_grade(s1, s2, s3):
     elif mean >= 60: return 'D'
     return 'F'
     
+
+# not my solution 
+def grade(s1, s2, s3):
+    return {6:'D', 7:'C', 8:'B', 9:'A', 10:'A'}.get((s1 + s2 + s3) / 30, 'F')
