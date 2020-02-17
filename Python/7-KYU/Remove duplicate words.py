24c618baa532fa36b4469cd1b82487dec4d0c692
yhertekin <yhertekin@gmail.com>
2020-02-17 19:45:09 +0300

    add remove duplicate words

---
+++ Python/7-KYU/Remove duplicate words.py
@@ -0,0 +1,18 @@
+"""
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+"""
+
+def remove_duplicate_words(s):
+    return ' '.join(sorted(set(s.split(' ')),key=s.index))
+   
+    
