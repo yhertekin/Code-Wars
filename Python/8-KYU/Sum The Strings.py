8b5dd69996f4b35423799d0ebd9a62b769975fe7
yhertekin <yhertekin@gmail.com>
2020-02-20 12:29:52 +0300

    add sum the strings

---
+++ Python/8-KYU/Sum The Strings.py
@@ -0,0 +1,13 @@
+"""
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+  sumStr("4", "5")    // should output "9"
+  sumStr("34", "5")   // should output "39"
+If either input is an empty string, consider it as zero.
+"""
+
+def sum_str(a, b):
+    if a == '': a = 0
+    if b == '': b = 0
+    
+    return str(int(a)+ int(b))
