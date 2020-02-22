c27092b50cc9f477ba80297ba869416e7287b7b2
yhertekin <yhertekin@gmail.com>
2020-02-22 23:30:01 +0300

    add I love you, a little , a lot, passionately ... not at all

---
+++ Python/8-KYU/I love you, a little , a lot, passionately ... not at all.py
@@ -0,0 +1,18 @@
+"""Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+"""
+
+def how_much_i_love_you(nb_petals):
+    phrases = {1: "I love you", 2: "a little", 3: "a lot", 4: "passionately" , 5: "madly", 0: "not at all"}
+    return phrases[nb_petals % 6]
+  
+    
