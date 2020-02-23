50f4ba1b6de8efbad74cd0382b53cfb49ebd93ba
yhertekin <yhertekin@gmail.com>
2020-02-23 20:03:04 +0300

    add 101 Dalmatians - squash the bugs, not the dogs!

--- Python/8-KYU/101 Dalmatians - squash the bugs, not the dogs!.py
+++ Python/8-KYU/101 Dalmatians - squash the bugs, not the dogs!.py
@@ -6,7 +6,7 @@ By repairing the function provided, you will find out exactly how you should res
 The number of dogs will always be a number and there will always be at least 1 dog.
 """
 def how_many_dalmatians(number):
-    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
   
     respond = dogs[0] if number <= 10 else dogs[1] if number <= 50 else dogs[3] if number == 101 else dogs[2]
 
