82a3e41029020bc1a4c875b0e6bfd34b156f20b7
yhertekin <yhertekin@gmail.com>
2020-02-26 20:28:19 +0300

    add Sum of Minimums!

---
+++ Python/7-KYU/Sum of Minimums!.py
@@ -0,0 +1,16 @@
+"""
+Given an 2D list of size m * n. Your task is to find the sum of minimum value in each row.
+
+For Example:
+
+[
+  [1,2,3,4,5], # minimum value of row is 1
+  [5,6,7,8,9], # minimum value of row is 5
+  [20,21,34,56,100] # minimum value of row is 20
+]
+So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+Note: You will be always given non-empty list containing Positive values.
+"""
+def sum_of_minimums(numbers):
+    return sum([min(num) for num in numbers])
