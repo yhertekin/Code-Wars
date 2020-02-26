226a6ae770fc1576bb6fe4c4a6aa92c1733f2c8d
yhertekin <yhertekin@gmail.com>
2020-02-26 20:24:53 +0300

    add very even numbers

---
+++ Python/7-KYU/Very Even Numbers.py
@@ -0,0 +1,29 @@
+"""
+Description:
+
+#Task:
+
+Write a function that returns true if the number is a "Very Even" number.
+
+If a number is a single digit, then it is simply "Very Even" if it itself is even.
+
+If it has 2 or more digits, it is "Very Even" if the sum of it's digits is "Very Even".
+
+#Examples:
+
+input(88) => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+
+input(222) => returns true
+
+input(5) => returns false
+
+input(841) => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+Note: The numbers will always be positive!
+"""
+def is_very_even_number(n):
+    my_num = sum([int(num) for num in str(n)])
+    if my_num > 9:
+        return is_very_even_number(my_num)
+    else:
+        return my_num % 2 == 0
+        
