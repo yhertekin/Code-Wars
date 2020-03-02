7c9054471bfd863f1a771e4baaf6c38c402c0565
yhertekin <yhertekin@gmail.com>
2020-03-02 23:50:33 +0300

    add Beginner - Reduce but Grow

---
+++ Python/8-KYU/Beginner - Reduce but Grow.py
@@ -0,0 +1,8 @@
+"""
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+"""
+from numpy import prod
+def grow(arr):
+    return prod(arr)
