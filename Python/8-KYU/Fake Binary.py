b6088d6134c775dd7060d112cf3321fde361ed1d
yhertekin <yhertekin@gmail.com>
2020-03-03 22:14:24 +0300

    add Fake Binary

---
+++ Python/8-KYU/Fake Binary.py
@@ -0,0 +1,5 @@
+"""
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+"""
+def fake_bin(x):
+    return "".join("0" if int(i) < 5 else "1" for i in list(x))
