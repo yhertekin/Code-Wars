ea949ca58843258763f8eec349c0380384bb8123
yhertekin <yhertekin@gmail.com>
2020-03-12 23:26:43 +0300

    add Function 3 multiplying two numbers

---
+++ Python/8-KYU/Function 3 - multiplying two numbers.py
@@ -0,0 +1,7 @@
+"""
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+"""
+def multiply(a,b):
+    return a*b
+
+
