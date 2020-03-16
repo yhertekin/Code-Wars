58a8db6d2f1aa03ee046c24b352e8bbd6987cea3
yhertekin <yhertekin@gmail.com>
2020-03-16 22:19:02 +0300

    add Grasshopper - Messi Goals

---
+++ Python/8-KYU/Grasshopper - Messi Goals.py
@@ -0,0 +1,24 @@
+"""
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 games
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Define
+Create three variables and store the appropriate values using the table above:
+
+la_liga_goals
+champions_league_goals
+copa_del_rey_goals
+Create a fourth variable named totalGoals (javascript, C#) or total_goals (python, ruby). totalGoals/total_goals should store the sum of all of Messi's goals for this year.
+"""
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
