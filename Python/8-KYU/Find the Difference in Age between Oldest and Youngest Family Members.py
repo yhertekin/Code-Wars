c95aab1ab7455c497ec17ae4442a76d15f67cfd8
yhertekin <yhertekin@gmail.com>
2020-03-18 16:39:42 +0300

    add Find the Difference in Age between Oldest and Youngest Family Members

---
+++ Python/8-KYU/Find the Difference in Age between Oldest and Youngest Family Members.py
@@ -0,0 +1,8 @@
+"""
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+"""
+def difference_in_ages(ages):
+    ages.sort()
+    return (ages[0], ages[-1], ages[-1]-ages[0])
