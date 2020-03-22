e5a212158b0d9d767fed81b33d0260cd41de30c2
yhertekin <yhertekin@gmail.com>
2020-03-22 23:20:15 +0300

    add FIXME: Replace all dots

---
+++ Python/8-KYU/FIXME: Replace all dots.py
@@ -0,0 +1,14 @@
+"""
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+"""
+
+def replace_dots(s):
+    return s.replace('.', '-')
