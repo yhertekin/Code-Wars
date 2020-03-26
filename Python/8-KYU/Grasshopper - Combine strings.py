86952135106ac59675a912311c0c1d23cd978af8
yhertekin <yhertekin@gmail.com>
2020-03-26 18:31:18 +0300

    Grasshopper - Combine strings

---
+++ Python/8-KYU/Grasshopper - Combine strings.py
@@ -0,0 +1,13 @@
+"""
+Combine strings function
+Create a function named combineNames(combine_names in python, ruby, CombineNames in C#) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combine_names('James', 'Stevens')
+returns:
+
+'James Stevens'
+"""
+
+combine_names = lambda x,y: x + ' ' + y
