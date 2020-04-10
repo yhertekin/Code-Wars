95d660adab99574fa6d486325decf7a2e213dcc3
yhertekin <yhertekin@gmail.com>
2020-04-10 20:59:48 +0300

    SQL Basics: Mod

---
+++ SQL/8-KYU/SQL Basics: Mod.sql
@@ -0,0 +1,13 @@
+/*
+Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.
+*/
+/*  SQL  */
+SELECT MOD(number1, number2)
+FROM decimals
