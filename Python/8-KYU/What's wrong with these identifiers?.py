1ccce01c0729f282faa1168ec87717a50e2ad684
yhertekin <yhertekin@gmail.com>
2020-04-30 17:12:20 +0300

    What's wrong with these identifiers?

---
+++ Python/8-KYU/What's wrong with these identifiers?.py
@@ -0,0 +1,10 @@
+"""
+An identifier is simply a name...
+Can you amend this object so that its properties comprise only vaild identifiers?
+"""
+Person = {
+  '1stname': "John",
+  'second-name': "Doe",
+  'email@ddress': "john.doe@email.com",
+  'male.female': "M"
+}
