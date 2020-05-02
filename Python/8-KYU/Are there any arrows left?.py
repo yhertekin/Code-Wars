a8369de282c15dc8b0486f49706b478174e6ed42
yhertekin <yhertekin@gmail.com>
2020-05-02 19:12:43 +0300

    Are there any arrows left?

---
+++ Python/8-KYU/Are there any arrows left?.py
@@ -0,0 +1,16 @@
+"""
+Check your arrows
+You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+You need to verify that you have some good ones left, in order to prepare for battle:
+
+anyArrows([{'range': 5}, {'range': 10, 'damaged': True}, {'damaged': True}])
+If an arrow in the quiver does not have a damaged status, it means it's new.
+
+The expected result is a boolean, indicating whether you have any good arrows left.
+"""
+
+def any_arrows(arrows):
+    for obj in arrows:
+        if obj.get('damaged', False) == False:
+            return True
+    return False
