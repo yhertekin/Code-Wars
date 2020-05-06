b2d1cfea11a5238b30303cbed436693df5839e5f
yhertekin <yhertekin@gmail.com>
2020-05-06 23:52:33 +0300

    Return the first M multiples of N

---
+++ Python/7-KYU/Return the first M multiples of N.py
@@ -0,0 +1,13 @@
+"""
+mplement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5.0)
+
+should return
+
+[5.0, 10.0, 15.0]
+"""
+def multiples(m, n):
+    return [n*i for i in range(1, m+1)]
