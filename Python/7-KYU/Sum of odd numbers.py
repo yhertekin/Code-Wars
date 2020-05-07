c8d0fd07d3696192041da029363de8451804f39b
yhertekin <yhertekin@gmail.com>
2020-05-07 22:02:06 +0300

    Sum of odd numbers

---
+++ Python/7-KYU/Sum of odd numbers.py
@@ -0,0 +1,25 @@
+"""
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+row_sum_odd_numbers(1); # 1
+row_sum_odd_numbers(2); # 3 + 5 = 8
+"""
+
+
+
+def sum_all(n):
+    return (n*(n+1)/2)**2
+
+def row_sum_odd_numbers(n):
+    return sum_all(n)-sum_all(n-1)
+    #or
+    # return n**3
